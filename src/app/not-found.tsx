da5902112cedc1833b083db506a0223919abeb41
Itztiva <tiva@Itztiva.com>
2025-03-13 22:40:00 -0400

    custom 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Sidebar from '@/components/core/SideBar'
+
+export default async function NotFound() {
+    return (
+        <div className="flex min-h-screen">
+            <Sidebar page={{ page: "Home" }} />
+            <div className="flex-1 flex flex-col items-center justify-center">
+                <div className="w-96 p-8 h-auto rounded-xl bg-white/5 backdrop-blur-lg border border-white/20 shadow-lg flex flex-col items-center justify-center">
+                    <div className="flex justify-center mb-2">
+                        <img
+                            src="./SolarisLogo.png"
+                            alt="Solaris Logo"
+                            className="h-20 w-auto object-contain"
+                        />
+                    </div>
+                    <h1 className="text-2xl font-lg">Page Not Found</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
