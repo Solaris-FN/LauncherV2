65d67a3092a831bad1d4847cba44172717d28b5e
Itztiva <tiva@Itztiva.com>
2025-03-29 09:52:11 -0400

    leaderboard

--- src/api/config/endpoints.ts
+++ src/api/config/endpoints.ts
@@ -12,6 +12,7 @@ export const endpoints = {
   GET_LAUNCHER_SERVERS: `${baseUrl}/s/api/v2/launcher/servers`,
   GET_LAUNCHER_POSTS: `${baseUrl}/s/api/v2/launcher/posts`,
   GET_LAUNCHER_SHOP: `${baseUrl}/s/api/v2/launcher/shop`,
+  GET_LAUNCHER_LEADERBOARD: `${baseUrl}/s/api/v2/launcher/leaderboard`,
 
   // account
   GET_GENERATE_ACCOUNT_RESP: `${baseUrl}/s/api/v2/launcher/account`,
