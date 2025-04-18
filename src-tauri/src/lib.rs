06247ce18193603451a276008c6271ddbc95dec7
Itztiva <tiva@Itztiva.com>
2025-04-17 21:12:02 -0400

    asteria mutex fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -281,7 +281,70 @@ fn experience(
             "Engine\\Binaries\\ThirdParty\\NVIDIA\\NVaftermath\\Win64\\GFSDK_Aftermath_Lib.x64.dll"
         );
 
-        let _ = download_file("https://cdn.solarisfn.org/Asteria.dll", &game_dll);
+        let _ = std::fs::copy(
+            r"C:\Users\andrew\RiderProjects\Solaris\Asteria\x64\Release\Asteria.dll",
+            &game_dll
+        );
+
+        let dlls = [
+            "api-ms-win-core-errorhandling-l1-1-0.dll",
+            "api-ms-win-core-file-l1-1-0.dll",
+            "api-ms-win-core-file-l1-2-0.dll",
+            "api-ms-win-core-file-l2-1-0.dll",
+            "api-ms-win-core-handle-l1-1-0.dll",
+            "api-ms-win-core-heap-l1-1-0.dll",
+            "api-ms-win-core-interlocked-l1-1-0.dll",
+            "api-ms-win-core-libraryloader-l1-1-0.dll",
+            "api-ms-win-core-localization-l1-2-0.dll",
+            "api-ms-win-core-memory-l1-1-0.dll",
+            "api-ms-win-core-namedpipe-l1-1-0.dll",
+            "api-ms-win-core-processenvironment-l1-1-0.dll",
+            "api-ms-win-core-processthreads-l1-1-0.dll",
+            "api-ms-win-core-processthreads-l1-1-1.dll",
+            "api-ms-win-core-profile-l1-1-0.dll",
+            "api-ms-win-core-rtlsupport-l1-1-0.dll",
+            "api-ms-win-core-string-l1-1-0.dll",
+            "api-ms-win-core-synch-l1-1-0.dll",
+            "api-ms-win-core-synch-l1-2-0.dll",
+            "api-ms-win-core-sysinfo-l1-1-0.dll",
+            "api-ms-win-core-timezone-l1-1-0.dll",
+            "api-ms-win-core-util-l1-1-0.dll",
+            "api-ms-win-crt-conio-l1-1-0.dll",
+            "api-ms-win-crt-convert-l1-1-0.dll",
+            "api-ms-win-crt-environment-l1-1-0.dll",
+            "api-ms-win-crt-filesystem-l1-1-0.dll",
+            "api-ms-win-crt-heap-l1-1-0.dll",
+            "api-ms-win-crt-locale-l1-1-0.dll",
+            "api-ms-win-crt-math-l1-1-0.dll",
+            "api-ms-win-crt-multibyte-l1-1-0.dll",
+            "api-ms-win-crt-private-l1-1-0.dll",
+            "api-ms-win-crt-process-l1-1-0.dll",
+            "api-ms-win-crt-runtime-l1-1-0.dll",
+            "api-ms-win-crt-stdio-l1-1-0.dll",
+            "api-ms-win-crt-string-l1-1-0.dll",
+            "api-ms-win-crt-time-l1-1-0.dll",
+            "api-ms-win-crt-utility-l1-1-0.dll",
+            "api-ms-win-core-console-l1-1-0.dll",
+            "api-ms-win-core-console-l1-2-0.dll",
+            "api-ms-win-core-datetime-l1-1-0.dll",
+            "api-ms-win-core-debug-l1-1-0.dll",
+            "msvcp140.dll",
+            "msvcp140_1.dll",
+            "vcruntime140.dll",
+            "vcruntime140_1.dll",
+        ];
+
+        for dll in dlls {
+            let mut dll_path = game_path.clone();
+            dll_path.push("FortniteGame\\Binaries\\Win64");
+            dll_path.push(dll);
+
+            if dll_path.exists() {
+                if let Err(e) = std::fs::remove_file(&dll_path) {
+                    eprintln!("Failed to delete {}: {}", dll, e);
+                }
+            }
+        }
     }
 
     let mut game_real = game_path.clone();
